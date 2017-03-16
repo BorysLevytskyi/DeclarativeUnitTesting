3f9538b585472eb2676430aafada6c6f2947b75f
Borys_Levytskyi <Borys_Levytskyi@epam.com>
2017-03-16 18:32:12 +0200

    Add some first working unit tests

--- src/Domain/Entities/UserStory.cs
+++ src/Domain/Entities/UserStory.cs
@@ -1,13 +1,19 @@
-﻿namespace Domain.Entities
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using Domain.Exceptions;
+
+namespace Domain.Entities
 {
     public class UserStory
     {
         private readonly UserStoryData _data;
 
-        public UserStory(string title)
+        public UserStory(int id, string title)
         {
             _data = new UserStoryData
             {
+                Id = id,
                 Title = title,
                 State = UserStoryState.InBacklog
             };
@@ -19,6 +25,8 @@
         }
 
         public int Id => _data.Id;
+
+        public int? SprintId => _data.SprintId;
 
         public StoryPoint? Estimate => _data.Estimate;
 
@@ -43,9 +51,15 @@
             _data.Assignee = assignee;
         }
 
-        public void Schedule()
+        public void Schedule(int sprintId)
         {
+            if (!Estimate.HasValue)
+            {
+                throw new UserStoryNotEstimatedException();
+            }
 
+            TransitionTo(UserStoryState.Scheduled);
+            _data.SprintId = sprintId;
         }
 
         public void Start()
@@ -75,6 +89,44 @@
         {
             return _data;
         }
+
+        private void TransitionTo(UserStoryState nextState)
+        {
+            if (!GetAllowedStateTransitions(State).Contains(nextState))
+            {
+                throw new InvalidUserStoryStateException(State, nextState);
+            }
+
+            _data.State = nextState;
+        }
+
+        private static IEnumerable<UserStoryState> GetAllowedStateTransitions(UserStoryState currentState)
+        {
+            switch (currentState)
+            {
+                case UserStoryState.InBacklog:
+                    yield return UserStoryState.Scheduled;
+                    break;
+                case UserStoryState.Scheduled:
+                    yield return UserStoryState.InProgress;
+                    yield return UserStoryState.InBacklog;
+                    break;
+                case UserStoryState.InProgress:
+                    yield return UserStoryState.InBacklog;
+                    yield return UserStoryState.Scheduled;
+                    yield return UserStoryState.Done;
+                    break;
+                case UserStoryState.Done:
+                    yield return UserStoryState.InProgress;
+                    yield return UserStoryState.Accepted;
+                    yield return UserStoryState.Rejected;
+                    break;
+                case UserStoryState.Accepted:
+                    yield break;
+                case UserStoryState.Rejected:
+                    yield break;
+            }
+        }
     }
 
     public class UserStoryData
@@ -88,5 +140,7 @@
         public string Title { get; set; }
 
         public int Id { get; set; }
+
+        public int? SprintId { get; set; }
     }
 }
